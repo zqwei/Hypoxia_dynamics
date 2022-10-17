5256541ac58899d00870e80e0a4b1556f69ad89f
Ziqiang Wei <weiz@janelia.hhmi.org>
2022-10-17 11:50:58 -0400

    add code to recompute camera locations

--- data/copyFile.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import shutil
-df = pd.read_csv('datalist.csv', index_col=0)
-
-for ind, row in df.iterrows():
-    ephys_ = row['dir_'] + '/ephys/analysis/'
-    save_root = row['save_root']
-    shutil.copyfile(ephys_+'data.mat', save_root+'/data.mat')
-    shutil.copyfile(ephys_+'locs_cam.mat', save_root+'/locs_cam.mat')
---
+++ data/locs_mat.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from h5py import File
+import os
+import shutil
+df = pd.read_csv('datalist.csv', index_col=0)
+
+for ind, row in df.iterrows():
+    ephys_ = row['dir_'] + '/ephys/analysis/'
+    save_root = row['save_root']
+    shutil.copyfile(ephys_+'data.mat', save_root+'/data.mat')
+    # shutil.copyfile(ephys_+'x3.mat', save_root+'/x3.mat')
+    # shutil.copyfile(ephys_+'locs_cam.mat', save_root+'/locs_cam.mat')
+    
+    x3 = File(ephys_ + 'x3.mat')['x3'][()].squeeze()
+    locs_cam = np.where((x3[:-1]<3.8) & (x3[1:]>3.8))[0]+1
+    # shape_ = np.load(save_root + 'cell_dff.npz', allow_pickle=True)['dFF'].shape[1]
+    # print(locs_cam.shape, shape_)
+    np.save(save_root + 'locs_cam.npy', locs_cam.astype('int'))
